176d3b328abbbfe2f84dcbe08e9221771e820cbf
Jommo Thuy <jthuy@su-f3.msk.21-school.ru>
2020-12-22 17:03:45 +0300

    add random eat and player params

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Vector2 mousePosition;
+    private Vector2 randVec;
     public float mass;
+    private float speed;
+    private Vector3 vecScale;
     // Start is called before the first frame update
     void Start()
     {
         mass = 10;
+        speed = 4;
+        vecScale.Set(1, 1, 1);
     }
 
     // Update is called once per frame
@@ -18,15 +23,19 @@ public class PlayerController : MonoBehaviour
         mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
         mousePosition -= (Vector2)transform.position;
-        transform.Translate(mousePosition * Time.deltaTime);
+        transform.Translate(mousePosition * Time.deltaTime * speed);
+        transform.localScale = vecScale;
     }
 
     void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.gameObject.tag == "Eat")
         {
-            mass += 1;
-            Destroy(collider.gameObject);
+            mass += 10;
+            speed *= 0.95f;
+            vecScale.Set((mass / 200 + 0.95f), (mass / 200 + 0.95f), 1);
+            randVec.Set(Random.Range(-25f, 25f), Random.Range(-25f, 25f));
+            collider.gameObject.transform.position = randVec;
         }
     }
 }
---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+    public GameObject eat;
+    private Vector2 randVector;
+
+    void Awake()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            randVector.Set(Random.Range(-25f, 25f), Random.Range(-25f, 25f));
+            Instantiate(eat, randVector, Quaternion.identity);
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
