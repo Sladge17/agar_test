2b2f84800db4e5ed1ba25d4a1573eb16df4cf99e
Jommo Thuy <jthuy@su-f3.msk.21-school.ru>
2020-12-22 16:00:45 +0300

    create simple scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector2 mousePosition;
+    public float mass;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mass = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        mousePosition -= (Vector2)transform.position;
+        transform.Translate(mousePosition * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Eat")
+        {
+            mass += 1;
+            Destroy(collider.gameObject);
+        }
+    }
+}
