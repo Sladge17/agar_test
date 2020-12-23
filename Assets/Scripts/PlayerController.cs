01eb0fe25b8e05dfd85f9c8a5959190a75f88d5d
Jommo Thuy <jthuy@su-f3.msk.21-school.ru>
2020-12-23 14:26:30 +0300

    add random scale for eat

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,47 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    Vector2 mousePosition;
-    private Vector2 randVec;
-    public float mass;
-    private float speed;
-    private Vector3 vecScale;
+    Vector2 vec2mousepos;
+    public float playermass;
+    private float playerspeed;
+    private Vector3 vec3playerscale;
     public Camera camera;
+
+    private Vector2 vec2eatpos;
+    private float factoreatscale;
+    private Vector3 vec3eatscale;
     
     // Start is called before the first frame update
     void Start()
     {
-        mass = 0;
-        speed = 4;
-        vecScale.Set(1, 1, 1);
+        playermass = 0;
+        playerspeed = 4;
+        vec3playerscale.Set(1, 1, 1);
         camera.orthographicSize = 4;
     }
 
     // Update is called once per frame
     void Update()
     {
-        mousePosition = Input.mousePosition;
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        mousePosition -= (Vector2)transform.position;
-        transform.Translate(mousePosition * Time.deltaTime * speed);
-        transform.localScale = vecScale;
+        vec2mousepos = Input.mousePosition;
+        vec2mousepos = Camera.main.ScreenToWorldPoint(vec2mousepos);
+        vec2mousepos -= (Vector2)transform.position;
+        transform.Translate(vec2mousepos * Time.deltaTime * playerspeed);
     }
 
     void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.gameObject.tag == "Eat")
         {
-            mass += 10;
-            speed *= 0.95f;
-            vecScale.Set((mass / 200 + 0.95f), (mass / 200 + 0.95f), 1);
-            randVec.Set(Random.Range(-25f, 25f), Random.Range(-25f, 25f));
-            collider.gameObject.transform.position = randVec;
-            if (mass % 100 == 0)
+            if (collider.transform.localScale[0] < vec3playerscale[0])
             {
-                camera.orthographicSize *= 1.02f;
+                playermass += 10;
+                playerspeed *= 0.95f;
+                vec3playerscale.Set((playermass / 200 + 0.95f), (playermass / 200 + 0.95f), 1);
+                transform.localScale = vec3playerscale;
+                vec2eatpos.Set(Random.Range(-25f, 25f), Random.Range(-25f, 25f));
+                collider.gameObject.transform.position = vec2eatpos;
+                factoreatscale = Random.Range(0.2f, 4f);
+                vec3eatscale.Set(factoreatscale, factoreatscale, 1);
+                collider.gameObject.transform.localScale = vec3eatscale;
+                if (playermass % 100 == 0)
+                {
+                    camera.orthographicSize *= 1.02f;
+                }
+            }
+            else
+            {
+                SceneManager.LoadScene("menu");
             }
         }
     }
--- Assets/Scripts/SpawnScript.cs
+++ Assets/Scripts/SpawnScript.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class SpawnScript : MonoBehaviour
 {
     public GameObject eat;
-    private Vector2 randVector;
+    private Vector2 vec2pos;
+    private float factorscale;
+    private Vector3 vec3scale;
 
     void Awake()
     {
         for (int i = 0; i < 100; i++)
         {
-            randVector.Set(Random.Range(-25f, 25f), Random.Range(-25f, 25f));
-            Instantiate(eat, randVector, Quaternion.identity);
+            vec2pos.Set(Random.Range(-25f, 25f), Random.Range(-25f, 25f));
+            Instantiate(eat, vec2pos, Quaternion.identity);
+            factorscale = Random.Range(0.2f, 4f);
+            vec3scale.Set(factorscale, factorscale, 1);
+            eat.transform.localScale = vec3scale;
         }
     }
     
