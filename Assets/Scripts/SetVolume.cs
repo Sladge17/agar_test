df0a1f036f335a493eb4bca7ef33d908899d1e17
Jommo Thuy <jthuy@su-g3.msk.21-school.ru>
2020-12-24 19:55:27 +0300

    add sound on/off

--- Assets/Scripts/SetVolume.cs
+++ Assets/Scripts/SetVolume.cs
@@ -11,6 +11,15 @@ public class SetVolume : MonoBehaviour
     {
         mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
     }
+
+    public void ToggleLevel(bool toggleCond)
+    {
+        if (toggleCond == false)
+            SetLevel(0.0001f);
+        else
+            SetLevel(0.5f);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
